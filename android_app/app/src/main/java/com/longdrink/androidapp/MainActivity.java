4b84a205f73a466d7a87c3e0d96cf051f5f8f2f2
clch2409 <cesarlch2409@gmail.com>
2023-08-01 12:36:16 -0500

    Creacion de los Fragments y TabLayout
    Agregando el TabLayout que permite cambiar entre Fragments

--- android_app/app/src/main/java/com/longdrink/androidapp/MainActivity.java
+++ android_app/app/src/main/java/com/longdrink/androidapp/MainActivity.java
@@ -1,16 +1,30 @@
 package com.longdrink.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.longdrink.androidapp.adapter.FragmentAdapter;
+import com.longdrink.androidapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    Toolbar myToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        myToolBar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolBar);
+
+        binding.viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        binding.menuTabs.setupWithViewPager(binding.viewPager);
     }
 
     @Override
---
+++ android_app/app/src/main/java/com/longdrink/androidapp/adapter/FragmentAdapter.java
@@ -0,0 +1,55 @@
+package com.longdrink.androidapp.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.longdrink.androidapp.fragments.CoursesFragment;
+import com.longdrink.androidapp.fragments.MyAccountFragment;
+import com.longdrink.androidapp.fragments.MyCoursesFragment;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+    public FragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 1: return new MyCoursesFragment();
+            case 2: return new MyAccountFragment();
+            default: return new CoursesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = null;
+
+        if (position == 0)
+        {
+            title = "Cursos";
+        }
+        if (position == 1)
+        {
+            title = "Mis Cursos";
+        }
+        if (position == 2)
+        {
+            title = "Mi Cuenta";
+        }
+
+        return title;
+    }
+}
