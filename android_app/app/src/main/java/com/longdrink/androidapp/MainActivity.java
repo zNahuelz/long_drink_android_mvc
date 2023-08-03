f06c92d9342ae18b3dd7b550f2a124594d992c24
clch2409 <cesarlch2409@gmail.com>
2023-08-03 11:41:24 -0500

    Pequeños cambios

--- android_app/app/src/main/java/com/longdrink/androidapp/MainActivity.java
+++ android_app/app/src/main/java/com/longdrink/androidapp/MainActivity.java
@@ -2,33 +2,38 @@ package com.longdrink.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
 import com.longdrink.androidapp.adapter.FragmentAdapter;
 import com.longdrink.androidapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
     Toolbar myToolBar;
+    ViewPager myViewPager;
+    TabLayout myTableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
+        myViewPager = findViewById(R.id.viewPager);
+        myTableLayout = findViewById(R.id.menu_tabs);
         myToolBar = findViewById(R.id.main_toolbar);
-        setSupportActionBar(myToolBar);
 
-        binding.viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
-        binding.menuTabs.setupWithViewPager(binding.viewPager);
+        setSupportActionBar(myToolBar);
+        myViewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        myTableLayout.setupWithViewPager(myViewPager);
     }
 
     @Override
     public void onBackPressed(){
         Toast.makeText(this, "Guiño guiño!", Toast.LENGTH_SHORT).show();
     }
+
 }
